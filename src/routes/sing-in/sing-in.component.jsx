7a2067058255d17d7c64bca211230aca8259f2cc
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-04-01 23:47:27 +0300

    sing in page

--- src/routes/sing-in/sing-in.component.jsx
+++ src/routes/sing-in/sing-in.component.jsx
@@ -1,15 +1,37 @@
-import { signInWithGooglePopup , createUserDocumentFromAuth  } from "../../util/firebase/firebase.util"
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
+
+import { 
+  auth,
+  signInWithGooglePopup ,
+   createUserDocumentFromAuth ,
+   signInWithGoogleRedirect
+ } from "../../util/firebase/firebase.util"
 
 
 const SignIn = () => {
+  useEffect(() => {
+    async () => {
+      const response = await getRedirectResult(auth)
+      if(response){
+        const userDocRef = await createUserDocumentFromAuth(response.user)
+      }
+    }
+  } , [])
+
   const logGoogleUser = async () => {
     const {user} = await signInWithGooglePopup();
-    createUserDocumentFromAuth(user)
+    const userDocRef = await createUserDocumentFromAuth(user)
+  };
+  const logGoogleUserRedirect = async () => {
+    const {user } = await signInWithGoogleRedirect()
   }
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with google account</button>
+      {/* <button onClick={logGoogleUser}>Sign in with google popup</button> */}
+      <button onClick={logGoogleUserRedirect}>Sign in with google redirect</button>
     </div>
   )
 }
