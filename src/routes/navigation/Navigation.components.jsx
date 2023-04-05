b8a48fb06b1621c528dc321d1e60f14f34826e18
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-04-06 00:28:47 +0300

    shop page and cart bag checkout feature

--- src/routes/navigation/Navigation.components.jsx
+++ src/routes/navigation/Navigation.components.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React ,{ useContext } from 'react'
 import { Outlet , Link} from 'react-router-dom'
+import { userContext } from '../../contexts/user.context'
+import { SignOutUser } from '../../util/firebase/firebase.util'
+import CardIcon from '../../Components/cart/cart.component'
+import CartDropdown from '../../Components/cardDropdow/cardDropdown.component'
+import { CartContext } from '../../contexts/cart.context'
 
 import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
 
+
 const Navigation = ()=> {
+  const {currentUser } = useContext(userContext)
+  const {isCartOpen} = useContext(CartContext)
+
   return (
     <React.Fragment>
       <div className='w-full h-[70px] flex justify-between mb-[25px]'>
@@ -12,10 +21,16 @@ const Navigation = ()=> {
         </Link>
         <div className='w-1/2 h-full flex items-center justify-end'>
           <Link className='px-[15px] py-2.5 cursor-pointer' to = "/shop"> SHOP </Link>
-          <Link className='px-[15px] py-2.5 cursor-pointer'> SHOP </Link>
-          <Link className='px-[15px] py-2.5 cursor-pointer' to = "/signIn"> SIGN IN </Link>
-          <Link className='px-[15px] py-2.5 cursor-pointer'> SHOP </Link>
+          <Link className='px-[15px] py-2.5 cursor-pointer'> CONTACT </Link>
+          {currentUser ? (
+            <span className='px-[15px] py-2.5 cursor-pointer' onClick={SignOutUser} > SIGN OUT</span>
+          ) : (
+            <Link className='px-[15px] py-2.5 cursor-pointer' to = "/signIn"> SIGN IN </Link>
+          )}
+          {/* <Link className='px-[15px] py-2.5 cursor-pointer'> SHOP </Link> */}
+          <CardIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
           <Outlet />
     </React.Fragment>
