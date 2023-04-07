b64e98d0e1e4cce206d2f496b032b821722a0bc2
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-04-07 23:42:42 +0300

    added routes for different products pages

--- src/routes/sing-in/sing-in.component.jsx
+++ src/routes/sing-in/sing-in.component.jsx
@@ -1,37 +1,15 @@
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
 
-import { 
-  auth,
-  signInWithGooglePopup ,
-   createUserDocumentFromAuth ,
-   signInWithGoogleRedirect
- } from "../../util/firebase/firebase.util"
+
  import SignUpForm from "../../Components/sign-up-form/sign-up-form.component";
+ import SignInAccount from "../../Components/sign-in-account/sign-in-account.component";
 
 
 const SignIn = () => {
-  useEffect(() => {
-    async () => {
-      const response = await getRedirectResult(auth)
-      if(response){
-        const userDocRef = await createUserDocumentFromAuth(response.user)
-      }
-    }
-  } , [])
 
-  const logGoogleUser = async () => {
-    const {user} = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user)
-  };
-  const logGoogleUserRedirect = async () => {
-    const {user } = await signInWithGoogleRedirect()
-  }
   return (
-    <div>
-      <h1>Sign In Page</h1>
+    <div className="flex w-[900px] justify-between my-[30px] mx-auto">
       {/* <button onClick={logGoogleUser}>Sign in with google popup</button> */}
-      <button onClick={logGoogleUserRedirect}>Sign in with google redirect</button>
+      <SignInAccount/>
       <SignUpForm/>
     </div>
   )
