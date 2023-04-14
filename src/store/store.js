27f0d65b8532d71db6eb8ef4e9b7e712fcb703dd
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-04-14 21:38:08 +0300

    production : netlify build

--- src/store/store.js
+++ src/store/store.js
@@ -4,17 +4,23 @@ import { rootReducer } from "./root-reducer";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { loggerMiddleware } from "./middleware/logger";
+// import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./root-saga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const persistConfig = {
   key: "root",
   storage,
 
   //values that you dont want in local storage
-  blackList: ["user"],
+  whiteList: ["cart"],
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 const middleWars = [
   process.env.NODE_ENV !== "productoin" && loggerMiddleware,
+  sagaMiddleware,
 ].filter(Boolean);
 
 const composeEnhancers =
@@ -30,4 +36,6 @@ export const store = legacy_createStore(
   undefined,
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 export const persister = persistStore(store);
