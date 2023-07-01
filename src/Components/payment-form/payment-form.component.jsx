0788cf9758e036a2e4d1a508bb88f6ab6a4c3926
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-07-01 23:59:59 +0300

    payment form init

--- src/Components/payment-form/payment-form.component.jsx
+++ src/Components/payment-form/payment-form.component.jsx
@@ -1,11 +1,10 @@
-import { CardElement , useStripe , useElements } from "@stripe/react-stripe-js";
-import Button , {BUTTON_TYPE_CLASSES} from "../button/button.component";
-import { PaymentFormContainer , FormContainer } from "./payment-form.styles";
-
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
 
 const PaymentForm = () => {
-  const stripe = useStripe()
-  const elements = useElements()
+  const stripe = useStripe();
+  const elements = useElements();
 
   const paymentHandler = async (e) => {
     e.preventDefault();
@@ -13,29 +12,27 @@ const PaymentForm = () => {
       return;
     }
     // setIsProcessingPayment(true);
-    const response = await fetch('/.netlify/functions/create-payment-intent', {
-      method: 'post',
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ amount: 10000 }),
     }).then((res) => {
       return res.json();
     });
-    console.log(response)
-  }
+    console.log(response);
+  };
 
-
-  return  (
+  return (
     <PaymentFormContainer>
       <FormContainer onSubmit={paymentHandler}>
-        <h2>Credit Card Payment:  </h2>
-      <CardElement/>
-      <Button buttonType={BUTTON_TYPE_CLASSES.inverted} > Pay now </Button>
+        <h2>Credit Card Payment: </h2>
+        <CardElement />
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay now </Button>
       </FormContainer>
     </PaymentFormContainer>
-  )
+  );
+};
 
-}
-
-export default PaymentForm
+export default PaymentForm;
