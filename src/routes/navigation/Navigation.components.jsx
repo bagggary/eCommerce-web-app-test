ba79b2dd833568667e35c393343090b6668d463a
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-07-03 23:37:07 +0300

    convert to ts

--- src/routes/navigation/Navigation.components.jsx
+++ src/routes/navigation/Navigation.components.jsx
@@ -1,45 +1,56 @@
-import React  from 'react'
-import { Outlet , Link} from 'react-router-dom'
-import { SignOutUser } from '../../util/firebase/firebase.util'
-import CardIcon from '../../Components/cart/cart.component'
-import CartDropdown from '../../Components/cardDropdow/cardDropdown.component'
-import { useSelector } from 'react-redux'
-import { selectIsCartOpen } from '../../store/cart/cart.selector'
-import { useDispatch } from 'react-redux'
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import { SignOutUser } from "../../util/firebase/firebase.util";
+import CardIcon from "../../Components/cart/cart.component";
+import CartDropdown from "../../Components/cardDropdow/cardDropdown.component";
+import { useSelector } from "react-redux";
+import { selectIsCartOpen } from "../../store/cart/cart.selector";
+import { useDispatch } from "react-redux";
 
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { signOutStart } from "../../store/user/user.action";
 
-import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
-import { signOutStart } from '../../store/user/user.action'
-
-
-const Navigation = ()=> {
-  const dispatch = useDispatch()
+const Navigation = () => {
+  const dispatch = useDispatch();
   // const currentUser = useSelector((state) => state.user.currentUser)
-  const currentUser = useSelector((state) => state.currentUser )
-  const isCartOpen = useSelector(selectIsCartOpen)
-  const handleSignOut = () => dispatch(signOutStart())
+  const currentUser = useSelector((state) => state.currentUser);
+  const isCartOpen = useSelector(selectIsCartOpen);
+  const handleSignOut = () => dispatch(signOutStart());
+  const handleSignIn = () => dispatch(sign);
 
   return (
     <React.Fragment>
-      <div className='w-full h-[70px] flex justify-between mb-[25px]'>
-        <Link className='h-full w-[70px] p-[25px]' to='/'> 
-        <CrwnLogo />
+      <div className="w-full h-[70px] flex justify-between mb-[25px]">
+        <Link className="h-full w-[70px] p-[25px]" to="/">
+          <CrwnLogo />
         </Link>
-        <div className='w-1/2 h-full flex items-center justify-end'>
-          <Link className='px-[15px] py-2.5 cursor-pointer' to = "/shop"> SHOP </Link>
-          <Link className='px-[15px] py-2.5 cursor-pointer'> CONTACT </Link>
+        <div className="w-1/2 h-full flex items-center justify-end">
+          <Link className="px-[15px] py-2.5 cursor-pointer" to="/shop">
+            {" "}
+            SHOP{" "}
+          </Link>
+          <Link className="px-[15px] py-2.5 cursor-pointer"> CONTACT </Link>
           {currentUser ? (
-            <span className='px-[15px] py-2.5 cursor-pointer' onClick={handleSignOut} > SIGN OUT</span>
+            <span
+              className="px-[15px] py-2.5 cursor-pointer"
+              onClick={handleSignOut}
+            >
+              {" "}
+              SIGN OUT
+            </span>
           ) : (
-            <Link className='px-[15px] py-2.5 cursor-pointer' to = "/signIn"> SIGN IN </Link>
+            <Link className="px-[15px] py-2.5 cursor-pointer" to="/signIn">
+              {" "}
+              SIGN IN{" "}
+            </Link>
           )}
           {/* <Link className='px-[15px] py-2.5 cursor-pointer'> SHOP </Link> */}
           <CardIcon />
         </div>
         {isCartOpen && <CartDropdown />}
       </div>
-          <Outlet />
+      <Outlet />
     </React.Fragment>
-  )
-}
-export default Navigation
+  );
+};
+export default Navigation;
